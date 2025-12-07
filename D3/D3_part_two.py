fe021a270e556b28a3b0fb9ae6091e17d9815a9e
Kivstahem <hiddelaagland@outlook.com>
2025-12-07 16:58:14 +0100

    D3

--- D3/D3_part_two.py
+++ D3/D3_part_two.py
@@ -1,27 +1,17 @@
 import time
-
 starttime = time.time()
-
 total_joltage = 0;
-
 BATTERY_BANK_SIZE = 12;
-
-# def dial(dial_current, number):
-#     new_dial_mod = (dial_current+number)%(maxDial+1);
-#     return new_dial_mod
 
 global output
 global output_index
 
-# FUCK YOU
 def push_down(output, output_index, index, limit, new_nmbr, new_nmbr_index):
     # check if we are not past the end:
     if index >= limit:
         return; # We are finished
-    
     old_nmbr       = output[index];
     old_nmbr_index = output_index[index];
-    
     if old_nmbr <= new_nmbr:
         # The new number is bigger or just as big, so let's replace it and push the current number down the stack
         if old_nmbr_index > new_nmbr_index:
@@ -34,19 +24,14 @@ def push_down(output, output_index, index, limit, new_nmbr, new_nmbr_index):
 
 with open("input.txt") as file:
     for line in file:
-        str_number     = line.strip();
-        # print("Original Battery", str_number)
+        str_number   = line.strip();
         output_index = [];
         output = list(line.strip()[-BATTERY_BANK_SIZE:]);
-        # output = list("0"*BATTERY_BANK_SIZE)
         start = len(str_number) - BATTERY_BANK_SIZE;
         output_index = list(range(start,start+BATTERY_BANK_SIZE));
         index          = output_index[0];
         for x in reversed(range(0, index)):
             push_down(output,output_index,0,BATTERY_BANK_SIZE,str_number[x],x)
-            
-        # print("Final Battery: ", int("".join(output)));
-        # print("Final index: ", output_index);
         total_joltage = total_joltage + int("".join(output)); 
 
 endtime = time.time()
